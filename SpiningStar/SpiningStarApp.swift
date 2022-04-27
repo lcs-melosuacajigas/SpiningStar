2ebef0a98b8efb120f9663a048e1a6becb829ab0
Mateo Elosua <melosua@lcs.on.ca>
2022-04-27 09:45:45 -0400

    Initial Commit

---
+++ SpiningStar/SpiningStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiningStarApp.swift
+//  SpiningStar
+//
+//  Created by Mateo Elosua on 2022-04-27.
+//
+
+import SwiftUI
+
+@main
+struct SpiningStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
